9b1dbce0ee683f759c7673864b96e6a7513d7db0
ManuelBava <manuelbava1996@gmail.com>
2021-02-19 22:03:49 +0100

    implementato automa

--- CRUD-Gal/src/crud/gal/CrudAutoma.java
+++ CRUD-Gal/src/crud/gal/CrudAutoma.java
@@ -26,36 +26,49 @@ public class CrudAutoma implements State {
     public class Initial implements State {
 
         public Initial() {
+            ui.statoIniziale();
         }
 
         @Override
         public void next(Event e) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            if (e instanceof InitialEvent) {
+                stato = new Ricerca();
+            }
         }
     }
 
     public class Ricerca implements State {
 
         public Ricerca() {
+            ui.entraStatoRicerca();
         }
 
         @Override
         public void next(Event e) {
-            if (e instanceof Ricerca) {
+            if (e instanceof RicercaEvent) {
                 stato = new Ricerca();
+            } else if (e instanceof SelezionaEvent) {
+                stato = new Visualizza();
+            } else if (e instanceof AddEvent) {
+                stato = new Aggiungi();
             }
-        }
 
+        }
     }
 
     private class Aggiungi implements State {
 
         public Aggiungi() {
+            ui.entraStatoAggiungi();
         }
 
         @Override
         public void next(Event e) {
-
+            if (e instanceof ConfermaEvent) {
+                stato = new Visualizza();
+            } else if (e instanceof AnnullaEvent) {
+                stato = new Ricerca();
+            }
         }
 
     }
@@ -63,22 +76,32 @@ public class CrudAutoma implements State {
     private class Visualizza implements State {
 
         public Visualizza() {
+            ui.entraStatoVisualizza();
         }
 
         @Override
         public void next(Event e) {
-
+            if (e instanceof ModificaEvent) {
+                stato = new Modifica();
+            } else if (e instanceof RicercaEvent) {
+                stato = new Ricerca();
+            }
         }
-
     }
 
     private class Modifica implements State {
 
         public Modifica() {
+            ui.entraStatoModifica();
         }
 
         @Override
         public void next(Event e) {
+            if (e instanceof AnnullaEvent) {
+                stato = new Visualizza();
+            } else if (e instanceof ConfermaEvent) {
+                stato = new Visualizza();
+            }
         }
 
     }
@@ -86,11 +109,16 @@ public class CrudAutoma implements State {
     private class Rimuovi implements State {
 
         public Rimuovi() {
+            ui.entraStatoRimuovi();
         }
 
         @Override
         public void next(Event e) {
-
+            if (e instanceof AnnullaEvent) {
+                stato = new Visualizza();
+            } else if (e instanceof ConfermaEvent) {
+                stato = new Ricerca();
+            }
         }
 
     }
